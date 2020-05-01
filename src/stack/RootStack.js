52f449f5e8a59788203c3604406f6c522644af5e
KashanMehmood <kashan.mehmood13@gmail.com>
2020-05-01 14:19:11 +0500

    Bottom tab navigator fixed

--- src/stack/RootStack.js
+++ src/stack/RootStack.js
@@ -3,12 +3,12 @@ import { View, Text, StyleSheet } from 'react-native';
 
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-// import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 
 import LoginComponent from '../component/LoginComponent';
-// import ContactComponent from '../component/ContactComponent';
+import ContactComponent from '../component/ContactComponent';
 import MessagesComponent from '../component/MessagesComponent';
-// import NotificationComponent from '../component/NotificationComponent';
+import NotificationComponent from '../component/NotificationComponent';
 
 class RootStack extends Component {
     render() {
@@ -28,41 +28,30 @@ const styles = StyleSheet.create({
     }
 });
 
-// const Tabnavigator = createMaterialBottomTabNavigator({
+const Tabnavigator = createMaterialBottomTabNavigator({
 
-//     Messages: { 
-//         screen: MessagesComponent, 
-//         navigationOptions: {
-//             tabBarLabel: 'Messages'        
-//         }
-//     },
+    Messages: { 
+        screen: MessagesComponent, 
+        navigationOptions: {
+            tabBarLabel: 'Messages'        
+        }
+    },
 
-//     Contact: { 
-//         screen: ContactComponent, 
-//         navigationOptions: {
-//             tabBarLabel: 'Contact'        
-//         }
-//     },
+    Contact: { 
+        screen: ContactComponent, 
+        navigationOptions: {
+            tabBarLabel: 'Contact'        
+        }
+    },
 
-//     Notification: { 
-//         screen: NotificationComponent, 
-//         navigationOptions: {
-//             tabBarLabel: 'Notification'        
-//         }
-//     },
-// });
+    Notification: { 
+        screen: NotificationComponent, 
+        navigationOptions: {
+            tabBarLabel: 'Notification'        
+        }
+    },
+});
 
-
-// const AppNavigator = createStackNavigator({
-//     Home: { 
-//         screen: LoginComponent,
-//         navigationOptions:{
-//             headerShown:false
-//         }
-//     },
-//   });
-
-// export default createAppContainer(AppNavigator); 
 
 const Stacknavigator = createStackNavigator({
     LoginScreen: {
@@ -71,8 +60,9 @@ const Stacknavigator = createStackNavigator({
             headerShown: false
         }
     },
+    
     Home: {
-        screen: MessagesComponent,
+        screen: Tabnavigator,
         navigationOptions: {
             headerShown: false
         }
